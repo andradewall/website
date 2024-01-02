721758feb8fc9a9720e861367dc384714873579a
Wallace Andrade <andradewall0@gmail.com>
2024-01-02 20:17:36 -0300

    chore: create hero section

---
+++ resources/views/sections/hero.blade.php
@@ -0,0 +1,12 @@
+<section class="flex justify-between items-center">
+    <div>
+        <h1 class="text-3xl font-bold mb-4">
+            Hello, I'm Wallace Andrade
+        </h1>
+        <p>
+            A full-stack web developer from Brazil 🇧🇷
+            <br />which loves to solve problems with PHP and coffee ☕
+        </p>
+    </div>
+    <x-draws.coding />
+</section>
