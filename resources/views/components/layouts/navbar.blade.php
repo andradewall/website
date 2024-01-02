6f90cf16cb43ed0b9e2c3590c3f3f265ebba2719
Wallace Andrade <andradewall0@gmail.com>
2024-01-02 19:35:09 -0300

    chore: create navbar structure

---
+++ resources/views/components/layouts/navbar.blade.php
@@ -0,0 +1,7 @@
+<nav class="bg-transparent">
+    <div class="flex justify-between max-w-screen-lg mx-auto py-8">
+        <x-navbar.logo />
+        <x-navbar.links />
+        <x-change-theme />
+    </div>
+</nav>
