cc96f1fdf9137a741dc598182c326d71038fabff
Wallace Andrade <andradewall0@gmail.com>
2024-01-02 19:38:28 -0300

    chore: add navbar links

---
+++ resources/views/components/navbar/links.blade.php
@@ -0,0 +1,6 @@
+<ul class="flex items-center uppercase space-x-4">
+    <x-navbar.link text="About" href="/" fragment="about"/>
+    <x-navbar.link text="Techs" href="/" fragment="techs"/>
+    <x-navbar.link text="Experience" href="/" fragment="experience"/>
+    <x-navbar.link text="Contact" href="/" fragment="contact"/>
+</ul>
