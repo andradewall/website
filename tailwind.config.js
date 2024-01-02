eee601b177a71f24c4622ef4b612e94676bb4d82
Wallace Andrade <andradewall0@gmail.com>
2024-01-02 16:42:26 -0300

    build: install TallStackUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+import forms from '@tailwindcss/forms';
+
 export default {
+  presets: [
+    require('./vendor/tallstackui/tallstackui/tailwind.config.js')
+  ],
+
   content: [
     "./resources/**/*.blade.php",
+    './vendor/tallstackui/tallstackui/src/**/*.php',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [forms],
 }
 
