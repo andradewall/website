3fab1d561181d043f2c82141e286e85a68b3f8b5
Wallace Andrade <andradewall0@gmail.com>
2024-01-13 19:38:54 -0300

    feat(section): create contact section

---
+++ resources/views/sections/contact.blade.php
@@ -0,0 +1,24 @@
+<section class="max-w-screen-lg mx-auto py-16">
+    <x-layouts.section-header>
+        <x-slot name="title">
+            CONTACT
+        </x-slot>
+        <x-slot name="sentence">
+            Let's get in touch
+        </x-slot>
+    </x-layouts.section-header>
+
+    <div class="flex space-x-8 justify-center mt-8">
+        <x-button text="LINKEDIN"
+                  icon-name="linkedin"
+                  href="https://www.linkedin.com/in/wllc-dev/" />
+
+        <x-button text="GITHUB"
+                  icon-name="github"
+                  href="https://github.com/andradewall" />
+
+        <x-button text="E-MAIL"
+                  icon-name="mail"
+                  href="mailto:contact@wllc.dev" />
+    </div>
+</section>
