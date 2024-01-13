37885382baaf0d76314986b963c2a8b99168a2bf
Wallace Andrade <andradewall0@gmail.com>
2024-01-13 19:51:34 -0300

    feat(section): create footer

---
+++ resources/views/components/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="w-full bg-stone-700 text-stone-200 py-12 text-center">
+    This page was created by myself with
+    <x-ts-link href="https://laravel.com" text="Laravel"/>
+    and
+    <x-ts-link href="htts://tallstackui.com" text="TallStackUI"/>
+</footer>
