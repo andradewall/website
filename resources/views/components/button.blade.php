474bd0cbebef709b68e4e31fc3b448de7d012408
Wallace Andrade <andradewall0@gmail.com>
2024-01-13 21:29:14 -0300

    style(component): wrap button component into a div

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -4,7 +4,9 @@
     'text',
 ])
 
-<x-ts-button href="{{ $href }}" color="orange" target="_blank">
-    <x-dynamic-component component="icons.{{ $iconName }}" class="w-6 h-6 mr-2 text-white"/>
-    {{ $text }}
-</x-ts-button>
+<div>
+    <x-ts-button href="{{ $href }}" color="orange" target="_blank">
+        <x-dynamic-component component="icons.{{ $iconName }}" class="w-6 h-6 mr-2 text-white"/>
+        {{ $text }}
+    </x-ts-button>
+</div>
