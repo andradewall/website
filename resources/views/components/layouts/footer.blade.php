6faf65a000f34b7cf6835b37f3ce1be82a55766f
Wallace Andrade <andradewall0@gmail.com>
2024-01-13 21:31:52 -0300

    feat(footer): improve footer

--- resources/views/components/layouts/footer.blade.php
+++ resources/views/components/layouts/footer.blade.php
@@ -1,6 +1,10 @@
-<footer class="w-full bg-stone-700 text-stone-200 py-12 text-center">
+<footer class="w-full bg-stone-700 text-stone-200 py-8 text-center">
     This page was created by myself with
     <x-ts-link href="https://laravel.com" text="Laravel"/>
     and
     <x-ts-link href="htts://tallstackui.com" text="TallStackUI"/>
+    using
+    <x-ts-link href="https://undraw.co" text="unDraw"/> illustrations
+    and
+    <x-ts-link href="https://phosphoricons.com" text="Phosphor Icons"/>
 </footer>
