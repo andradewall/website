1287ac06d5eabd63a05881b5e5fd1a3cfe5f9d5a
Wallace Andrade <andradewall0@gmail.com>
2024-01-13 21:30:18 -0300

    style: change scrollbar

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}"
       x-data="tallstackui_darkTheme()"
-      x-bind:class="{ 'dark bg-gray-700': darkTheme, 'bg-white': !darkTheme }">
+      x-bind:class="{ 'dark bg-gray-700': darkTheme, 'bg-white': !darkTheme }"
+      class="scroll-smooth custom-scrollbar">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
