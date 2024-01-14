3df57cfdfebe308faafb990c2f5622a550ad784d
Wallace Andrade <andradewall0@gmail.com>
2024-01-13 21:31:15 -0300

    feat(contact): improve contact section

--- resources/views/sections/contact.blade.php
+++ resources/views/sections/contact.blade.php
@@ -8,17 +8,22 @@
         </x-slot>
     </x-layouts.section-header>
 
-    <div class="flex space-x-8 justify-center mt-8">
-        <x-button text="LINKEDIN"
-                  icon-name="linkedin"
-                  href="https://www.linkedin.com/in/wllc-dev/" />
+    <div class="h-[70dvh] space-y-6 flex flex-col justify-center items-center mt-8">
+        <div>
+            You can chat with me on LinkedIn, see my projects on GitHub or e-mail me at contact@wllc.dev
+        </div>
+        <div class="flex space-x-8">
+            <x-button text="LINKEDIN"
+                      icon-name="linkedin"
+                      href="https://www.linkedin.com/in/wllc-dev/" />
 
-        <x-button text="GITHUB"
-                  icon-name="github"
-                  href="https://github.com/andradewall" />
+            <x-button text="GITHUB"
+                      icon-name="github"
+                      href="https://github.com/andradewall" />
 
-        <x-button text="E-MAIL"
-                  icon-name="mail"
-                  href="mailto:contact@wllc.dev" />
+            <x-button text="E-MAIL"
+                      icon-name="mail"
+                      href="mailto:contact@wllc.dev" />
+        </div>
     </div>
 </section>
