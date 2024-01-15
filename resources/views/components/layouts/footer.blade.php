81ea070f735a0a2e820d8ddc5c9a651d27a6c9b5
Wallace Andrade <andradewall0@gmail.com>
2024-01-15 11:48:54 -0300

    fix(footer): fix the wrong tallstackui url

--- resources/views/components/layouts/footer.blade.php
+++ resources/views/components/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     This page was created by myself with
     <x-ts-link href="https://laravel.com" text="Laravel"/>
     and
-    <x-ts-link href="htts://tallstackui.com" text="TallStackUI"/>
+    <x-ts-link href="https://tallstackui.com" text="TallStackUI"/>
     using
     <x-ts-link href="https://undraw.co" text="unDraw"/> illustrations
     and
