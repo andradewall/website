8dd1f3285ece40086bfe50a668b7740447750aac
Wallace Andrade <andradewall0@gmail.com>
2024-02-13 11:05:41 -0300

    style: add side padding for entire page

--- resources/views/components/layouts/navbar.blade.php
+++ resources/views/components/layouts/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="bg-transparent animation-fade">
-    <div class="flex justify-between max-w-screen-lg mx-auto py-8">
+    <div class="flex justify-between max-w-screen-lg px-4 mx-auto py-8">
         <x-navbar.logo />
         <x-navbar.links />
         <x-change-theme />
