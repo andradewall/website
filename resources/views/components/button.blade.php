21a4650d7c7b2babc29c7ce780baf384cf3ed365
Wallace Andrade <andradewall0@gmail.com>
2024-02-14 15:05:20 -0300

    style: change button default size to large

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -5,7 +5,7 @@
 ])
 
 <div>
-    <x-ts-button href="{{ $href }}" color="orange" target="_blank">
+    <x-ts-button lg href="{{ $href }}" color="orange" target="_blank">
         <x-dynamic-component component="icons.{{ $iconName }}" class="w-6 h-6 mr-2 text-white"/>
         {{ $text }}
     </x-ts-button>
