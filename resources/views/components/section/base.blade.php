722f24893491d9a539034941c02ae3c8bba4266d
Wallace Andrade <andradewall0@gmail.com>
2024-02-14 15:30:31 -0300

    style: tidy Tailwind classes

--- resources/views/components/section/base.blade.php
+++ resources/views/components/section/base.blade.php
@@ -1,4 +1,4 @@
-<section id="{{ Str::lower($title) }}" class="mx-auto flex h-dvh max-w-screen-lg flex-col py-16 px-4">
+<section id="{{ Str::lower($title) }}" class="mx-auto flex h-dvh max-w-screen-lg flex-col px-4 py-16">
     <x-section.header>
         <x-slot name="title">
             {{ $title}}
